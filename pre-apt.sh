c061be5dacc88f590addae1151e511e4d710f659
K.O <31298964+k5iogura@users.noreply.github.com>
2018-10-21 22:01:58 +0900

    Update pre-apt.sh

--- pre-apt.sh
+++ pre-apt.sh
@@ -4,8 +4,8 @@ sudo apt install -y git
 sudo apt upgrade -y
 sudo apt install -y python3 python
 sudo apt install -y python3-pip python-pip
-git clone https://github.com/k5iogura/ncsdk
-git clone https://github.com/k5iogura/ncappzoo
+# git clone https://github.com/k5iogura/ncsdk
+# git clone https://github.com/k5iogura/ncappzoo
 
 sudo apt install -y linusb-1.0
 sudo pip3 install numpy
