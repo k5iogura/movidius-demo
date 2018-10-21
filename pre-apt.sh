6dec87d9f0fd06fa4ea44a995fc15aa09ed19fb6
Kenji Ogura <k5i.ogura.v40@gmail.com>
2018-10-21 21:43:50 +0900

    revise

--- pre-apt.sh
+++ pre-apt.sh
@@ -2,8 +2,12 @@
 sudo apt update
 sudo apt install -y git
 sudo apt upgrade
+sudo apt install -y python3 python
 sudo apt install -y python3-pip python-pip
 git clone https://github.com/k5iogura/ncsdk
 git clone https://github.com/k5iogura/ncappzoo
 
+sudo apt install -y linusb-1.0
+sudo pip3 install numpy
+sudo pip3 install opencv-python opencv-contrib-python
 echo "Done"
