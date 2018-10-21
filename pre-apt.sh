0dc4b0639032d1a8431446aa73226a6e8d951abb
K.O <31298964+k5iogura@users.noreply.github.com>
2018-10-21 22:08:23 +0900

    Update pre-apt.sh

--- pre-apt.sh
+++ pre-apt.sh
@@ -7,7 +7,7 @@ sudo apt install -y python3-pip python-pip
 # git clone https://github.com/k5iogura/ncsdk
 # git clone https://github.com/k5iogura/ncappzoo
 
-sudo apt install -y linusb-1.0
+sudo apt install -y libusb-1.0
 sudo pip3 install numpy
 sudo pip3 install opencv-python opencv-contrib-python
 echo "Done"
